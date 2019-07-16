9229de21878309bda72b175af6d896721c474a32
Simon Gordon <gordonsimon@hotmail.com>
2019-07-16 20:27:30 +1000

    Loop through user input

--- src/index.py
+++ src/index.py
@@ -13,17 +13,27 @@ def parseDate(input_str):
             pass
     return None
 
+parsed_date = None
 
-date_str = input('When were you born? ')
-# date_str = '27 June 1996'
+while parsed_date is None:
+    date_str = input('When were you born? ')
+    # date_str = '27 June 1996'
+    parsed_date = parseDate(date_str);
+    if (parsed_date is None):
+        print('That\'s not a valid date!')
 
-parsed_date = parseDate(date_str);
 
-if parsed_date is None:
-    print('Invalid date!')
-else:
-    # print(dir(parsed_date))
-    month = parsed_date.month
-    day = parsed_date.day
-    print('Your day is',day,'and your month is',month)
+month = parsed_date.month
+day = parsed_date.day
+print('Your day is', day, 'and your month is', month)
+print('Tell me about yourself!')
+
+run_loop = True
+while run_loop:
+    user_input = input('> ')
+    if user_input == 'exit':
+        print('Bye!')
+        run_loop = False
+    else:
+        print('Wow!')
 

88257da14fc75586349dce53a64a7f5a31b14ec5
Simon Gordon <gordonsimon@hotmail.com>
2019-09-13 20:55:32 +1000

    Create React+next project

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from "next/head";
+import "./index.css";
+
+const Home = () => (
+  <>
+    <Head>
+      <title>Astrology chat</title>
+    </Head>
+
+    <h1>Astrology chat</h1>
+
+    <p>Welcome</p>
+  </>
+);
+
+export default Home;
